54cef4aa378537791d2037328811fede64218e3f
hiiamhip-laptop <trantuanhiep420@gmail.com>
2024-12-02 15:34:16 +0000

    second

--- main.go
+++ main.go
@@ -5,10 +5,19 @@ import (
 	"log"
 )
 
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.HandleFunc("/healthz", readinessHandler)
+
+	fileServer := http.FileServer(http.Dir("."))
+	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
 
 	server := &http.Server{
 		Addr:    ":8080",  
