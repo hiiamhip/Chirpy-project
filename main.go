da3c9bc29b621bef7574f61ba391c6cc296e7ad7
hiiamhip <trantuanhiep420@gmail.com>
2024-12-03 11:55:32 +0000

    third

--- main.go
+++ main.go
@@ -3,7 +3,95 @@ package main
 import (
 	"net/http"
 	"log"
+	"fmt"
+	"sync/atomic"
+	"encoding/json"
+	"strings"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(map[string]string{"error": msg})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(payload)
+}
+
+// increase request every time it's called
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func cleanChirpText(text string) string {
+	profaneWords := []string{"kerfuffle", "sharbert", "fornax"}
+
+	words := strings.Fields(text)
+
+	for i, word := range words {
+		for _, badWord := range profaneWords {
+			if strings.EqualFold(word, badWord) {
+				words[i] = "****"
+			}
+		}
+	}
+
+	return strings.Join(words, " ")
+}
+
+// Handler for /admin/metrics
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	hits := cfg.fileserverHits.Load()
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, `
+		<html>
+		  <body>
+		    <h1>Welcome, Chirpy Admin</h1>
+		    <p>Chirpy has been visited %d times!</p>
+		  </body>
+		</html>
+	`, hits)
+}
+
+// Handler for /admin/reset
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	fmt.Fprintf(w, "Counter reset")
+}
+
+// handler for /api/validate_chirp
+func handlervalidateChirp(w http.ResponseWriter, r *http.Request) {
+	type chirpBody struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	chirp := chirpBody{}
+	err := decoder.Decode(&chirp)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Something went wrong")
+		return
+	}
+
+	if len(chirp.Body) > 140 {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+		return
+	}
+
+	cleanedBody := cleanChirpText(chirp.Body)
+	respondWithJSON(w, http.StatusOK, map[string]string{"cleaned_body": cleanedBody})
+
+}
+
 
 func readinessHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -12,12 +100,17 @@ func readinessHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	apiCfg := &apiConfig{}
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/healthz", readinessHandler)
-
 	fileServer := http.FileServer(http.Dir("."))
-	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
+	handlerWithPrefix := http.StripPrefix("/app", fileServer)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handlerWithPrefix))
+
+	mux.HandleFunc("GET /api/healthz", readinessHandler)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+	mux.HandleFunc("/api/validate_chirp", handlervalidateChirp)
 
 	server := &http.Server{
 		Addr:    ":8080",  
