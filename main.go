1bcdeda24658af4cd1f161a72684db18281c55f2
hiiamhip <trantuanhiep420@gmail.com>
2024-12-04 08:02:06 +0000

    fifth

--- main.go
+++ main.go
@@ -1,144 +1,66 @@
 package main
 
 import (
-	"sync/atomic"
 	"database/sql"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"encoding/json"
-	"strings"
-	"github.com/joho/godotenv" // Import godotenv
-	_ "github.com/lib/pq"       // PostgreSQL driver
-	
+	"sync/atomic"
+
+	"chirpy/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
-}
-
-func respondWithError(w http.ResponseWriter, code int, msg string) {
-	w.WriteHeader(code)
-	json.NewEncoder(w).Encode(map[string]string{"error": msg})
-}
-
-func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-	json.NewEncoder(w).Encode(payload)
-}
-
-// increase request every time it's called
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
-
-func cleanChirpText(text string) string {
-	profaneWords := []string{"kerfuffle", "sharbert", "fornax"}
-
-	words := strings.Fields(text)
-
-	for i, word := range words {
-		for _, badWord := range profaneWords {
-			if strings.EqualFold(word, badWord) {
-				words[i] = "****"
-			}
-		}
-	}
-
-	return strings.Join(words, " ")
-}
-
-// Handler for /admin/metrics
-func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	hits := cfg.fileserverHits.Load()
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintf(w, `
-		<html>
-		  <body>
-		    <h1>Welcome, Chirpy Admin</h1>
-		    <p>Chirpy has been visited %d times!</p>
-		  </body>
-		</html>
-	`, hits)
-}
-
-// Handler for /admin/reset
-func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
-	cfg.fileserverHits.Store(0)
-	fmt.Fprintf(w, "Counter reset")
-}
-
-// handler for /api/validate_chirp
-func handlervalidateChirp(w http.ResponseWriter, r *http.Request) {
-	type chirpBody struct {
-		Body string `json:"body"`
-	}
-
-	decoder := json.NewDecoder(r.Body)
-	chirp := chirpBody{}
-	err := decoder.Decode(&chirp)
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Something went wrong")
-		return
-	}
-
-	if len(chirp.Body) > 140 {
-		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
-		return
-	}
-
-	cleanedBody := cleanChirpText(chirp.Body)
-	respondWithJSON(w, http.StatusOK, map[string]string{"cleaned_body": cleanedBody})
-
-}
-
-
-func readinessHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
+	db             *database.Queries
+	platform       string
 }
 
 func main() {
-	err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
+	const filepathRoot = "."
+	const port = "8080"
 
-    dbURL := os.Getenv("DB_URL")
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL must be set")
+	}
+	platform := os.Getenv("PLATFORM")
+	if platform == "" {
+		log.Fatal("PLATFORM must be set")
+	}
 
-    db, err := sql.Open("postgres", dbURL)
-    if err != nil {
-        log.Fatal("Error opening database: ", err)
-    }
-    defer db.Close()
+	dbConn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error opening database: %s", err)
+	}
+	dbQueries := database.New(dbConn)
 
-	apiCfg := &apiConfig{}
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+		db:             dbQueries,
+		platform:       platform,
+	}
+
 	mux := http.NewServeMux()
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", fsHandler)
 
-	fileServer := http.FileServer(http.Dir("."))
-	handlerWithPrefix := http.StripPrefix("/app", fileServer)
-	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handlerWithPrefix))
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("POST /api/validate_chirp", handlerChirpsValidate)
 
-	mux.HandleFunc("GET /api/healthz", readinessHandler)
-	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
+
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
-	mux.HandleFunc("/api/validate_chirp", handlervalidateChirp)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 
-	server := &http.Server{
-		Addr:    ":8080",  
+	srv := &http.Server{
+		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	log.Println("Server is starting on http://localhost:8080")
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatalf("Server failed: %v", err)
-	}
-
-
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
 }
